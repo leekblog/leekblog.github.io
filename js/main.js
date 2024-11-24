eacc9018ca10ca9ff3ed9f24b3c11661b24ee8ba
leekblog <leekblog@proton.me>
2024-11-24 17:31:44 +0800

    'auto'

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ var dom =document.getElementsByTagName("footer")[0].querySelectorAll("a");
 
 for(let i=0;i<dom.length;i++){
     dom[i].onclick = ()=>{
-        alert("开发中...");
+        alert("Email: leekblog@proton.me");
     }
 }
