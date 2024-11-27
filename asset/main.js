0de49ba07a360fa42ebb63125f4c38e422422ba2
leekblog <leekblog@proton.me>
2024-11-27 17:10:54 +0800

    'update'

--- asset/main.js
+++ asset/main.js
@@ -2,7 +2,7 @@ const script = document.createElement('script');
 document.head.appendChild(script);
 script.onload = function() {
     if(document.querySelector("code.sourceCode") != null){
-        script.src = "https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/prism.min.js";
-        Prism.highlightAll();
+        script.src = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/highlight.min.js";
+        hljs.highlightAll();
     }
 };
