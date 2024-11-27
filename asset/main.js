cba7c4ea8d2993f26bd01b4e62a246e6476d5b23
leekblog <leekblog@proton.me>
2024-11-27 17:29:43 +0800

    'update'

--- asset/main.js
+++ asset/main.js
@@ -1,6 +1,8 @@
-if (document.querySelector("code.sourceCode") != null) {
-    const script = document.createElement('script');
-    script.src = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/highlight.min.js";
-    document.head.appendChild(script);
-    hljs.highlightAll();
-}
+script.onload = function() {
+    if(document.querySelector("code.sourceCode") != null){
+        const script = document.createElement('script');
+        script.src = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/highlight.min.js";
+        document.head.appendChild(script);
+        hljs.highlightAll();
+    }
+};
