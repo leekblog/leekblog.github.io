55b75852944678333e834c7583f2b82f4f7c34fc
leekblog <leekblog@proton.me>
2024-11-27 17:41:02 +0800

    'update'

--- asset/main.js
+++ asset/main.js
@@ -3,6 +3,6 @@ if (document.querySelector("code.sourceCode") != null) {
     script.src = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/highlight.min.js";
     document.head.appendChild(script);
     script.onload = function () {
-        // hljs.highlightAll()
+        hljs.highlightAll();
     };
 }
