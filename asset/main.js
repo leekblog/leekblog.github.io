8bbfdf779eb1e53d70d9fefaafaebbb98ed0f4d1
leekblog <leekblog@proton.me>
2024-11-27 17:26:37 +0800

    'update'

--- asset/main.js
+++ asset/main.js
@@ -1,8 +1,6 @@
-script.onload = function() {
-    if(document.querySelector("code.sourceCode") != null){
-        const script = document.createElement('script');
-        script.src = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/highlight.min.js";
-        document.head.appendChild(script);
-        hljs.highlightAll();
-    }
-};
+if (document.querySelector("code.sourceCode") != null) {
+    const script = document.createElement('script');
+    script.src = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/highlight.min.js";
+    document.head.appendChild(script);
+    hljs.highlightAll();
+}
